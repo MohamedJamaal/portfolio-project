dc77d84a3acb61aa1a98bf3e8184f7788158184c
MohamedJamaal <mohamed_gamal19@hotmail.com>
2019-02-02 17:15:36 +0200

    added Bootstrap and show jobs page

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+
+# Create blog model
+#title
+#pub-date
+#body
+#image
+
+class Blog(models.Model):
+    title = models.CharField(max_length=225)
+    pub_date = models.DateTimeField()
+    body = models.TextField()
+    image = models.ImageField(upload_to='images/')
+
+
+
+    def __str__(self):
+            return self.title
+# add the blog app to the settings
+
+
+# create migrations
+# migrate
+
+
+# add to admin
--- jobs/views.py
+++ jobs/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from .models import Job
 # Create your views here.
+
+def home(request):
+
+    jobs = Job.objects   # object from jobs model
+    return render(request , 'jobs/home.html' , {'jobs':jobs}) #dictionary off all jobs objects from jobs model
